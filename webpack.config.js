b66417fb2d99954f5a91dfd9169ad48e4792381a
Hiren Joshi <hirenj@gmail.com>
2019-10-14 12:42:49 +0200

    Add in project scaffold

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: {
+    'glycan': [ './js/pastemapper.js' ],
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+    {
+      test: /sugars\.svg$/,
+      use: 'raw-loader'
+    },
+    {
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            ['@babel/preset-env', {
+              modules: false,
+              corejs: 'core-js@2',
+              useBuiltIns: 'entry',
+              targets: {
+                browsers: [
+                  'Chrome >= 60',
+                  'Safari >= 10.1',
+                  'iOS >= 10.3',
+                  'Firefox >= 54',
+                  'Edge >= 15',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+};
